4556166198a746a93acd9cd8b53402a17e421139
Victor Zhang <vcz.zhang@gmail.com>
2020-04-15 22:53:38 -0700

    server changes

--- yikyak/server.js
+++ yikyak/server.js
@@ -62,11 +62,14 @@ app.use("/api/reply", reply)
 
 app.get('/', function (req, res) { res.send('pong') });
 
-// app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
