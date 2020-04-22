ac475804f98d39b72580d9ef4ee1e7e8c848a478
Victor Zhang <vcz.zhang@gmail.com>
2020-04-21 22:05:07 -0700

    landing page changes

--- yikyak/client/src/Components/LandingPage/Features.js
+++ yikyak/client/src/Components/LandingPage/Features.js
@@ -12,25 +12,29 @@ class Features extends Component {
 
     render() {
         return (
-            <div>
+            <div className='featuresPage'>
                 <div>
                     <Header />
                 </div>
-                <div>
-                    <div>
+                <div className='featuresLanding'>
+                    <div className='featuresLandingCopy'>
                         <h1>Get a Get a live feed of what people are saying around you.</h1>
                     </div>
                     <div>
-                        <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/timeline@2x.png' />
-                        <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/detail-header@2x.png' />
+                        <div className='featuresImgContainer'>
+                            <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/timeline@2x.png' />
+                        </div>
+                        <div className='featuresImgContainer'>
+                            <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/detail-header@2x.png' />
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/compose@2x.png' />
+                <div className='featuresChatSection'>
+                    <div className='featuresImgContainer'>
+                        <img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/phones/compose@2x.png' width='300px' />
                     </div>
                     <div>
-                        <div><img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/features/features-1-chatbubble-01.svg' /></div>
+                        <div className='featuresImgContainer'><img src='https://web.archive.org/web/20151210202710im_/http://www.yikyakapp.com/wp-content/themes/yik-yak-web-general/img/features/features-1-chatbubble-01.svg' width='100px' /></div>
                         <h1>Join the conversation.</h1>
                         <p>Share your thoughts with people around you.</p>
                     </div>
