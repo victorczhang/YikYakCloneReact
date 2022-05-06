83b0b0bf8b14854b17caa693d5601f48ab43eb1a
Victor Zhang <vcz.zhang@gmail.com>
2022-05-05 23:00:25 -0700

    Standing up local dev env

--- yikyak/server.js
+++ yikyak/server.js
@@ -68,9 +68,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('/*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+} else {
+  const path = require('path');
+  app.use(express.static(path.resolve(__dirname, 'client', 'public', 'index.html')));
 }
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
